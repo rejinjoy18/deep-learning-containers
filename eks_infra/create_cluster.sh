e1d6e2732b1e959565379411c66dc781fbb6735a
tejaschumbalkar <tejaschumbalkar@gmail.com>
2021-01-19 13:14:11 -0800

    delete cluster

--- eks_infra/create_cluster.sh
+++ eks_infra/create_cluster.sh
@@ -64,7 +64,7 @@ function create_node_group(){
     --asg-access \
     --ssh-access \
     --ssh-public-key "${3}"
-    '''
+    : '
     # dynamic inf nodegroup
     eksctl create nodegroup \
     --name inf-nodegroup-${2/./-} \
@@ -79,7 +79,7 @@ function create_node_group(){
     --asg-access \
     --ssh-access \
     --ssh-public-key "${3}"
-    '''
+    '
 }
 
 # Function to create namespaces in EKS cluster
